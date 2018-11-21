e86d5b1cb0b1446518cae8a030dff18c4cd15fd4
didid08 <diva.mujaddidi@outlook.com>
2018-11-21 17:59:13 +0700

    Updated

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ var http = require('http');
 var fs = require('fs');
 var MongoClient = require('mongodb').MongoClient;
 var config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
-
 var db_url = config.database.db_url;
+
 MongoClient.connect(db_url, { useNewUrlParser: true }, function(err, client) {
 	db = client.db(config.database.db_name);
 	db.collection("settings").findOne({}, function(err, result){
 		if (err) throw err;
+
+		// Settings
+		var settings = result;
 
 		http.createServer(function (req,res) {
 			if(req.url == "/"){
@@ -16,10 +19,11 @@ MongoClient.connect(db_url, { useNewUrlParser: true }, function(err, client) {
 				res.end();
 			}else if(req.url == "/admin"){
 				res.writeHead(200, {"Content-Type" : "text/html"});
-				res.write(fs.readFileSync("./data/resources/template/"+result.user.site.activetemplate+"/admin.fhtml", "utf-8").replace("[title]", result.user.site.title));
+				res.write(fs.readFileSync("./data/resources/template/"+settings.user.site.activetemplate+"/admin.fhtml", "utf-8").replace("[title]", settings.user.site.title));
 				res.end();
 			}
 		}).listen(config.webserver.port);
+
 		client.close();
 
 	});

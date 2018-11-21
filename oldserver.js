28903f15e1e34aa3abb4951c8ed796bd06daef57
didid08 <diva.mujaddidi@outlook.com>
2018-11-21 17:44:19 +0700

    Updated

---
+++ oldserver.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+
+var config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+
+function getValue(data, callback){
+	var db_url = config.database.db_url;
+	MongoClient.connect(db_url, { useNewUrlParser: true }, function(err, client) {
+		db = client.db(config.database.db_name);
+		db.collection("settings").findOne({}, function(err, result){
+			if (err) throw err;
+			data = result.user.site.title;
+		});
+		client.close();
+	});
+}
+function page(fileName){
+	var page = fs.readFileSync("./data/resources/template/default/"+fileName+".fhtml", "utf-8");
+	var newPage = page.replace("[title]", getValue(function (data){return data}));
+	return newPage;
+}
+
+http.createServer(function (req,res) {
+	if(req.url == "/"){
+		res.writeHead(200, {"Content-Type" : "text/html"});
+		res.write("Flashcms");
+		res.end();
+	}else if(req.url == "/admin"){
+		res.writeHead(200, {"Content-Type" : "text/html"});
+		res.write(page("admin"));
+		res.end();
+	}
+}).listen(config.webserver.port);
--- server.js
+++ server.js
@@ -1,36 +1,26 @@
 var http = require('http');
 var fs = require('fs');
 var MongoClient = require('mongodb').MongoClient;
-
 var config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
 
-function getValue(){
-	var resultt = "";
-	var db_url = config.database.db_url;
-	MongoClient.connect(db_url, { useNewUrlParser: true }, function(err, client) {
-		db = client.db(config.database.db_name);
-		db.collection("settings").findOne({}, function(err, result){
-			if (err) throw err;
-			resultt = result.user.site.title;
-		});
-		client.close();
-	});
-	return resultt;
-}
-function page(fileName){
-	var page = fs.readFileSync("./data/resources/template/default/"+fileName+".fhtml", "utf-8");
-	var newPage = page.replace("[title]", getValue());
-	return newPage;
-}
+var db_url = config.database.db_url;
+MongoClient.connect(db_url, { useNewUrlParser: true }, function(err, client) {
+	db = client.db(config.database.db_name);
+	db.collection("settings").findOne({}, function(err, result){
+		if (err) throw err;
 
-http.createServer(function (req,res) {
-	if(req.url == "/"){
-		res.writeHead(200, {"Content-Type" : "text/html"});
-		res.write("Flashcms");
-		res.end();
-	}else if(req.url == "/admin"){
-		res.writeHead(200, {"Content-Type" : "text/html"});
-		res.write(page("admin"));
-		res.end();
-	}
-}).listen(config.webserver.port);
+		http.createServer(function (req,res) {
+			if(req.url == "/"){
+				res.writeHead(200, {"Content-Type" : "text/html"});
+				res.write("Flashcms");
+				res.end();
+			}else if(req.url == "/admin"){
+				res.writeHead(200, {"Content-Type" : "text/html"});
+				res.write(fs.readFileSync("./data/resources/template/"+result.user.site.activetemplate+"/admin.fhtml", "utf-8").replace("[title]", result.user.site.title));
+				res.end();
+			}
+		}).listen(config.webserver.port);
+		client.close();
+
+	});
+});
